e9e5a2102d0c27b72c425aef5036d77f93081ae6
ArthurLai <arthur.csie@gmail.com>
2013-04-09 09:38:29 +0800

    modify appid and secret

--- facebook_sdk/examples/example.php
+++ facebook_sdk/examples/example.php
@@ -19,8 +19,8 @@ require '../src/facebook.php';
 
 // Create our Application instance (replace this with your appId and secret).
 $facebook = new Facebook(array(
-  'appId'  => '344617158898614',
-  'secret' => '6dc8ac871858b34798bc2488200e503d',
+  'appId'  => '478084965579094',
+  'secret' => '5e74063e6dc3ef0dfa2c738ad1e84c2f',
 ));
 
 // Get User ID
