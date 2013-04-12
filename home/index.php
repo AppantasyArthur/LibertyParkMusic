e0f931e518b6f2b8f705d6e44f3bd37fd211eb61
ArthurLai <arthur.csie@gmail.com>
2013-04-12 19:05:22 +0800

    slider modify new menu init

--- home/index.php
+++ home/index.php
@@ -7,6 +7,103 @@
 	
 	<link rel='stylesheet' href='../css/lpm-base.css'>
 	<link rel='stylesheet' href='../css/lpm-menu.css'>
+	<link rel='stylesheet' href='../css/lpm-slider.css'>
+	
+	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script src="http://code.jquery.com/jquery-migrate-1.1.1.min.js"></script>
+	<script type="text/javascript">
+	var legend_bg;
+	var i = 0;
+	var sliderInterval;
+	var slider_pics = ['../11.jpg', '../22.jpg', '../33.jpg'];
+	var slider_div_id = 'lpm-slider';
+	function sliderStart(){
+
+		sliderInterval = setInterval(function(){
+
+			// clear element
+			$('#' + slider_div_id).empty();
+
+			// creat / append / hide img
+			var img = $(document.createElement('img'));
+
+			if(i > slider_pics.length - 1)
+				i = 0;
+			var img_url = slider_pics[i++];
+			img.attr('src', img_url);
+			img.appendTo('#' + slider_div_id);
+			img.hide();
+
+			/*var event_block = $(document.createElement('div'));
+			event_block.addClass('event_block');
+			event_block.hover(hoverOnSlider, hoverOutSlider);
+			event_block.appendTo('#' + slider_div_id);
+			event_block.hide();*/
+			
+			legend_bg = $(document.createElement('div'));
+			legend_bg.addClass('legend_bg');
+			legend_bg.appendTo('#' + slider_div_id);
+			legend_bg.html('hello world, this is background');
+			legend_bg.hide();
+				
+			var legend_label = $(document.createElement('div'));
+			legend_label.addClass('legend_label');
+			legend_label.appendTo('#' + slider_div_id);
+			legend_label.hide();
+
+			
+			
+			// effect switching
+			// show img
+			img.fadeIn('slow');  // effect timing
+			legend_bg.fadeIn();
+			legend_label.fadeIn();
+			//event_block.fadeIn();
+			
+		}, 3 * 1000); // transmit timing
+
+	}
+
+	function sliderStop(){
+		clearInterval(sliderInterval);
+	}
+
+	function sliderlegendResizeLarger(){
+		legend_bg
+			.animate({
+			    height: 100
+			}, 500, function() {
+			    	// Animation complete.
+			});
+	}
+
+	function sliderlegendResizeSmaller(){
+		legend_bg
+			.delay(1000)
+			.animate({
+			    height: 30
+			}, 500, function() {
+			    // Animation complete.
+			});
+	}
+	
+	function hoverOnSlider(){
+		sliderStop();
+		sliderlegendResizeLarger();
+	}
+
+	function hoverOutSlider(){
+		sliderStart();
+		sliderlegendResizeSmaller();
+	}
+	
+	$(document).ready(function() {
+
+		$('#' + slider_div_id).hover(hoverOnSlider, hoverOutSlider);
+		sliderStart();
+
+	});
+	</script>
 	
 </head>
 
@@ -69,7 +166,7 @@
 		<div id='main-content'>
 		
 			<div class='display-section'>
-			
+				<div id='lpm-slider'></div>
 			</div><!-- display-section -->
 		
 		</div><!-- main-content -->
--- test/index.php
+++ test/index.php
@@ -6,99 +6,42 @@
 	<meta charset='utf-8'>
 	<title></title>
 	
-	<link rel="stylesheet" href="lpm-slider.css">
+	<link rel="stylesheet" href="lpm-menu-2.css">
 	
+	<script src="../javascript/json2.js"></script>
 	<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
 	<script src="http://code.jquery.com/jquery-migrate-1.1.1.min.js"></script>
 	<script type="text/javascript">
 
-		var legend_bg;
-		var i = 0;
-		var sliderInterval;
-		var slider_pics = ['../11.jpg', '../22.jpg', '../33.jpg'];
-		var slider_div_id = 'lpm-slider';
-		function sliderStart(){
-
-			sliderInterval = setInterval(function(){
-
-				// clear element
-				$('#' + slider_div_id).empty();
-
-				// creat / append / hide img
-				var img = $(document.createElement('img'));
-
-				if(i > slider_pics.length - 1)
-					i = 0;
-				var img_url = slider_pics[i++];
-				img.attr('src', img_url);
-				img.appendTo('#' + slider_div_id);
-				img.hide();
-
-				legend_bg = $(document.createElement('div'));
-				legend_bg.addClass('legend_bg');
-				legend_bg.appendTo('#' + slider_div_id);
-				legend_bg.html('hello world, this is background');
-				legend_bg.hide();
-					
-				var legend_label = $(document.createElement('div'));
-				legend_label.addClass('legend_label');
-				legend_label.appendTo('#' + slider_div_id);
-				legend_label.hide();
-
-				var event_block = $(document.createElement('div'));
-				event_block.addClass('event_block');
-				event_block.hover(hoverOnSlider, hoverOutSlider);
-				event_block.appendTo('#' + slider_div_id);
-				event_block.hide();
-				
-				// effect switching
-				// show img
-				img.fadeIn('slow');  // effect timing
-				legend_bg.fadeIn();
-				legend_label.fadeIn();
-				event_block.fadeIn();
-				
-			}, 3 * 1000); // transmit timing
-
-		}
-
-		function sliderStop(){
-			clearInterval(sliderInterval);
-		}
-
-		function sliderlegendResizeLarger(){
-			legend_bg
-				.animate({
-				    height: 100
-				}, 500, function() {
-				    	// Animation complete.
-				});
-		}
-
-		function sliderlegendResizeSmaller(){
-			legend_bg
-				.delay(1000)
-				.animate({
-				    height: 30
-				}, 500, function() {
-				    // Animation complete.
-				});
-		}
-		
-		function hoverOnSlider(){
-			sliderStop();
-			sliderlegendResizeLarger();
-		}
-
-		function hoverOutSlider(){
-			sliderStart();
-			sliderlegendResizeSmaller();
-		}
-		
+		var menu = {
+			//length: 3,
+			header: ['first', 'second', 'third'],
+			data:{
+				'first':['item_1', 'item_2'],
+				'second':['item_1', 'item_2'],
+				'third':['item_1', 'item_2']
+			}
+		};
+	
 		$(document).ready(function() {
 
-			sliderStart();
+			//var data = menu;
+			
+			for(var i = 0;i < menu.header.length;i++){
 
+				console.log(JSON.stringify(menu.data[menu.header[i]], null, 4));
+				
+			}
+			
+			//console.log(JSON.stringify(data, null, 4));
+			//console.log(JSON.stringify(data['second'], null, 4));
+
+			/*var legend_label = $(document.createElement('div'));
+			legend_label.addClass('legend_label');
+			legend_label.appendTo('#' + slider_div_id);
+			legend_label.hide();*/
+
+			
 		});
 		
 	</script>
@@ -106,11 +49,8 @@
 
 <body>
 	
-	<div id='lpm-slider'>
-		
-	</div>
+	<div id='lpm-menu'></div>
 	
-
 </body>
 
 </html>
