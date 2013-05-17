c0d2c200c7d57c1b8b9003b196e38dc3c2331d53
AppantasyArthur <appantasyarthur@hotmail.com>
2013-05-17 09:58:41 +0800

    123

--- lesson/getPdfText.php
+++ lesson/getPdfText.php
@@ -4,7 +4,7 @@
 	
 	include_once '../php_global/dbsetting.php';
 	
-	$filename = '/Appantasy/GitHubRepo/LibertyParkMusic/lesson/pdfTest.pdf';
+	$filename = '/Users/Appantasy/GitHubRepo/LibertyParkMusic/lesson/pdfTest.pdf';
 	$content = shell_exec('/usr/local/bin/pdftotext '.$filename.' -');
 	$response["pdftext"] = $content;
 	$response["filename"] = $_REQUEST['file'];
