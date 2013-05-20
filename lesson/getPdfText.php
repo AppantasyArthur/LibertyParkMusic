8276d625c08bc1405c834568299fd37d7ad58359
AppantasyArthur <appantasyarthur@hotmail.com>
2013-05-20 17:58:12 +0800

    open pdf local

--- lesson/getPdfText.php
+++ lesson/getPdfText.php
@@ -4,7 +4,8 @@
 	
 	include_once '../php_global/dbsetting.php';
 	
-	$filename = '/Users/Appantasy/GitHubRepo/LibertyParkMusic/lesson/pdfTest.pdf';
+	//$filename = '/Users/Appantasy/GitHubRepo/LibertyParkMusic/lesson/pdfTest.pdf';
+	$filename = '/Users/arthurlai/git/LibertyParkMusic/lesson/pdfTest.pdf';
 	$content = shell_exec('/usr/local/bin/pdftotext '.$filename.' -');
 	$response["pdftext"] = $content;
 	$response["filename"] = $_REQUEST['file'];
--- lesson/index.php
+++ lesson/index.php
@@ -598,29 +598,45 @@
 	var study_mode_hook; // =  $('#full-screen-hook');
 	function showStudyMode(){
 
-		var bodyHeight = $('body').height();
+		if(getAcrobatInfo().acrobat){
+			
+			//console.log('已安裝');
+
+			var bodyHeight = $('body').height();
+			
+			study_mode_hook =  $('#full-screen-hook');
+			study_mode_hook.show();
+			study_mode_hook.height(bodyHeight);
+
+			$('#workspace-hook').height(bodyHeight);
+
+			var pdfObject = $(document.createElement('object'));
+			// data="test.pdf" type="application/pdf" width="300" height="200"
+			pdfObject.attr('data', 'pdfTest.pdf');
+			pdfObject.attr('type', 'application/pdf');
+			pdfObject.width('100%');
+			pdfObject.height('800px');
+			//pdfObject.addClass('study-doc');
+			//pdfObject.css('background-color', 'black');
+
+			var anchor = $('#workspace-hook');
+			pdfObject.appendTo(anchor);
+			
+			scroll2Top();
+			
+		}else{
+			console.log('未安裝');
+			alert('Install Acrobat Reader First.');
+		}
 		
-		study_mode_hook =  $('#full-screen-hook');
-		study_mode_hook.show();
-		study_mode_hook.height(bodyHeight);
+	}
 
-		$('#workspace-hook').height(bodyHeight);
+	function scroll2Top(){
 
-		$.get(	
-				'getPdfText.php'
-				,{
-					'file':'pdfText.pdf'
-				}
-				,function(data, textStatus, jqXHR){
-
-					//console.log(JSON.stringify(data, null, 4));
-					//console.log(JSON.stringify(textStatus, null, 4));
-					//console.log(JSON.stringify(jqXHR, null, 4));
-
-					$('#workspace-hook').text(data.pdftext);
-
-				}
-		);
+		var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
+		$body.animate({
+			scrollTop: 0
+		}, 600);
 		
 	}
 
@@ -629,6 +645,79 @@
 		study_mode_hook.hide();
 		
 	}
+
+	var getAcrobatInfo = function() {
+		 
+		  var getBrowserName = function() {
+		    return this.name = this.name || function() {
+		      var userAgent = navigator ? navigator.userAgent.toLowerCase() : "other";
+		 
+		      if(userAgent.indexOf("chrome") > -1)        return "chrome";
+		      else if(userAgent.indexOf("safari") > -1)   return "safari";
+		      else if(userAgent.indexOf("msie") > -1)     return "ie";
+		      else if(userAgent.indexOf("firefox") > -1)  return "firefox";
+		      return userAgent;
+		    }();
+		  };
+		 
+		  var getActiveXObject = function(name) {
+		    try { return new ActiveXObject(name); } catch(e) {}
+		  };
+		 
+		  var getNavigatorPlugin = function(name) {
+		    for(key in navigator.plugins) {
+		      var plugin = navigator.plugins[key];
+		      if(plugin.name == name) return plugin;
+		    }
+		  };
+		 
+		  var getPDFPlugin = function() {
+		    return this.plugin = this.plugin || function() {
+		      if(getBrowserName() == 'ie') {
+		        //
+		        // load the activeX control
+		        // AcroPDF.PDF is used by version 7 and later
+		        // PDF.PdfCtrl is used by version 6 and earlier
+		        return getActiveXObject('AcroPDF.PDF') || getActiveXObject('PDF.PdfCtrl');
+		      }
+		      else {
+		        return getNavigatorPlugin('Adobe Acrobat') || getNavigatorPlugin('Chrome PDF Viewer') || getNavigatorPlugin('WebKit built-in PDF');
+		      }
+		    }();
+		  };
+		 
+		  var isAcrobatInstalled = function() {
+		    return !!getPDFPlugin();
+		  };
+		 
+		  var getAcrobatVersion = function() {
+		    try {
+		      var plugin = getPDFPlugin();
+		 
+		      if(getBrowserName() == 'ie') {
+		        var versions = plugin.GetVersions().split(',');
+		        var latest   = versions[0].split('=');
+		        return parseFloat(latest[1]);
+		      }
+		 
+		      if(plugin.version) return parseInt(plugin.version);
+		      return plugin.name
+		      
+		    }
+		    catch(e) {
+		      return null;
+		    }
+		  }
+		 
+		  //
+		  // The returned object
+		  // 
+		  return {
+		    browser:        getBrowserName(),
+		    acrobat:        isAcrobatInstalled() ? true : false,
+		    acrobatVersion: getAcrobatVersion()
+		  };
+		};
 	
 	</script>
 	
@@ -647,14 +736,17 @@
 				<div class='lesson-description'>In this lesson, you will learn about key signature for all keys, and the use of accidentals to after pitches. Major and minor modes will both be covered.</div>
 			</div>
 			<div class='study-mode-lesson-works'>
-				<div class='lesson-file-tab' onclick='showStudyMode();'>Homework</div>
+				<div class='lesson-file-tab'><a onclick='showStudyMode();'>Homework</a></div>
 				<div class='lesson-file-tab'>Share</div>
 				<div class='lesson-file-tab' onclick='showStudyNoteMode();'>Note</div>
 			</div>
 		</div>
 		<div class='study-mode-right'>
+			<a id='workspace-hook-anchor'></a>
 			<div id='workspace-hook' class='study-mode-lesson-workspace'>
-				
+				<!-- <object data="pdfTest.pdf" type="application/pdf" width="300" height="200">
+					
+				</object> -->
 				<!-- <di class='study-mode-note-input-block'>
 				</di>
 				<di class='study-mode-note-list'>
